b64c52e35cd0e3668c47a28f145a7bced6985bc0
Muhammad Hafiz <hafiz170301@gmail.com>
2024-11-11 14:59:14 +0700

    refactor: optimize access token and refresh token generator

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::SessionsController < ApplicationController
   ACCESS_TOKEN_SECRET  = ENV["ACCESS_TOKEN_SECRET"]
   REFRESH_TOKEN_SECRET = ENV["REFRESH_TOKEN_SECRET"]
+  TOKEN_EXPIRATION = {
+    access: 1.hours.from_now,
+    refresh: 7.hours.from_now
+  }.freeze
 
   def signup 
     @user = User.new(session_params)
 
-    @user.save ? response_ok(@user, 200) 
+    @user.save ? response_ok(@user, 201) 
       : response_error(@user.errors, 422)
   end
 
@@ -13,15 +17,10 @@ class Api::V1::SessionsController < ApplicationController
     @user = User.find_by(username: session_params[:username])
 
     if @user && @user.authenticate(session_params[:password])
-      access_token  = encode_user_data({ user_id: @user.id, exp: 1.hours.from_now.to_i }, ACCESS_TOKEN_SECRET)
-      refresh_token = encode_user_data({ user_id: @user.id, exp: 7.hours.from_now.to_i }, REFRESH_TOKEN_SECRET)
-
-      render json: {
-        message: "success",
-        access_token: access_token, 
-        refresh_token: refresh_token,
-        data: @user
-      }, status: 201
+      render_token({
+        access: generate_token(@user.id, TOKEN_EXPIRATION[:access], ACCESS_TOKEN_SECRET),
+        refresh: generate_token(@user.id, TOKEN_EXPIRATION[:refresh] ,REFRESH_TOKEN_SECRET)
+      })
     else
       response_error("Invalid username or password", 422)
     end
@@ -30,14 +29,31 @@ class Api::V1::SessionsController < ApplicationController
   def refresh_token 
     user_id = decode_user_data(params[:refresh_token], REFRESH_TOKEN_SECRET)
 
-    if user_id 
-      access_token  = encode_user_data({ user_id: user_id[0]["user_id"], exp: 1.hours.from_now.to_i }, ACCESS_TOKEN_SECRET) 
-      refresh_token = encode_user_data({ user_id: user_id[0]["user_id"], exp: 7.hours.from_now.to_i }, REFRESH_TOKEN_SECRET)
-
-      render json: { access_token: access_token, refresh_token: refresh_token}
+    if user_id
+      render_token({
+        access: generate_token(user_id[0]["user_id"], TOKEN_EXPIRATION[:access], ACCESS_TOKEN_SECRET),
+        refresh: generate_token(user_id[0]["user_id"], TOKEN_EXPIRATION[:refresh] ,REFRESH_TOKEN_SECRET)
+      })
     else
       response_error("Invalid token", 400)
     end
+  end
+
+  def render_token tokens
+    render json: {
+        message: "success",
+        data: {
+          access_token: tokens[:access], 
+          refresh_token: tokens[:refresh]
+        }
+      }, status: 201
+  end
+
+  def generate_token user_id, expiration, secret
+    {
+      token: encode_user_data({ user_id: user_id, exp: expiration.to_i }, secret),
+      exp_at: expiration
+    }
   end
 
   def session_params
